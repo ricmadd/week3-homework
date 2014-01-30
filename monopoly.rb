b3578a820794dbea9073fb84ae6e993a256646d4
RicardoMaduro <rmaduro@gmail.com>
2014-01-30 00:02:20 -0600

    Week3 Homework
    Monopoly mini-problems.

--- monopoly.rb
+++ monopoly.rb
@@ -3,9 +3,9 @@
 cheap_property = { :name => 'baltic', :price => 60, :rent => 2 }
 rich_property = { :name => 'boardwalk', :price => 400, :rent => 50 }
 more_properties = [ { :name => 'Virginia Ave', :price => 60, :rent => 10},
-                    { :name => 'Marvin Gardens', :price => 160, :rent => 20},
-                    { :name => 'New York Ave', :price => 180, :rent => 18},
-                   ]
+                                { :name => 'Marvin Gardens', :price => 160, :rent => 20},
+                                { :name => 'New York Ave', :price => 180, :rent => 18},
+                               ]
 
 special_squares = { :good => ["Go", "Free Parking", "Just Visiting"],
                     :bad => ["Go To Jail", "Luxury Tax"]
@@ -17,22 +17,22 @@ puts cheap_property[:price]
 
 #Q2. How much does it cost to buy Boardwalk?
 #A2: Your code goes here:
-
+puts rich_property[:price]
 
 
 #Q3. How much is rent for Baltic?
 #A3: Your code goes here:
-
+puts cheap_property[:rent]
 
 
 #Q4. How much does it cost to buy New York Ave?
 #A4: Your code goes here:
-
+puts  more_properties[2][:price]
 
 
 #Q5: How many "good" special squares are there?
 #A5: Your code goes here:
-
+puts special_squares[:good].count
 
 
 
